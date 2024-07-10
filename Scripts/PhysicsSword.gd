be8a5bfef6b7c0afbc49b87b34fe94b9673bb0cb
Justin Rodriguez <jrod9520@gmail.com>
2024-07-11 00:02:59 +0900

    basic paralax bg

--- Scripts/PhysicsSword.gd
+++ Scripts/PhysicsSword.gd
@@ -10,4 +10,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("swing"):
 		print("do something polz")
-		apply_force(Vector2.UP * 1000 , Vector2.ZERO)
+		apply_torque_impulse(-100000)
+	elif Input.is_action_just_pressed("swing_l"):
+		apply_torque_impulse(100000)
+
